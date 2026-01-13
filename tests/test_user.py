420ad9ec31c31b003051c32d1257bc903e571578
VelenskiyS <inferfire@gmail.com>
2026-01-13 17:51:13 +0300

    add tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,3 +1,5 @@
+from os import name
+
 from fastapi.testclient import TestClient
 
 from src.main import app
@@ -26,16 +28,43 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': 'nouser@gmail.com'})
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User not found'}
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    new_user = {
+        'name': 'New User',
+        'email': 'valid_email@gmail.com',
+    }
+    response = client.post("/api/v1/user", json=new_user)
+    assert response.status_code == 201
+    assert isinstance(response.json(), int)
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    existing_user = {
+        'name': 'New User',
+        'email': users[0]['email'],
+    }
+    response = client.post("/api/v1/user", json=existing_user)
+    assert response.status_code == 409
+    assert response.json() == {'detail': 'User with this email already exists'}
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+    temp_user = {
+        'name': 'Temp User',
+        'email': 'temp@gmail.com',
+    }
+    create_response = client.post("/api/v1/user", json=temp_user)
+    user = create_response.json()
+
+    #delete
+    delete_response = client.delete("/api/v1/user", params={"email": temp_user['email']})
+    assert delete_response.status_code == 204
+
+    #check
+    deleted_response = client.get("/api/v1/user", params={"email": temp_user['email']})
+    assert deleted_response.status_code == 404
